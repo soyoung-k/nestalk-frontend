ef941132e78aa0270fe3dabc03eabb6ab9f6a2d1
soyoung-k <soyoung.kk1@gmail.com>
2021-03-15 00:37:39 +0900

    [#31] feat: ant 디자인 로그인/로그아웃 폼 이용하여 임시 ui 구현

---
+++ src/components/pages/Login/Login.tsx
@@ -0,0 +1,57 @@
+type LoginProps = {};
+
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const Login: React.FC<LoginProps> = ({}) => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <div>
+      {' '}
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Useremail"
+          name="useremail"
+          rules={[{ required: true, message: 'Please input your useremail!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+export default Login;
